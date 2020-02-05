17910b593bbd2c225206e1062566ea2e28050a9d
Vincent <vleclerc@inow.fr>
2020-02-05 10:51:31 +0100

    add apikey

---
+++ pe2020/pe2020/Services/WeatherService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pe2020.Services
+{
+    public class WeatherService
+    {
+        static string API_KEY = "b40441c6c394a1074aa67f7cfd5de7d3";
+        static string URL = $"https://api.openweathermap.org/data/2.5/weather?appid={API_KEY}&units=metric";
+
+
+
+    }
+}
